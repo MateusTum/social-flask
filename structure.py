0fd3253ed8c733902223b9f1a42fead4aac1806d
MateusTum <83322634+MateusTum@users.noreply.github.com>
2023-11-23 20:09:15 -0300

    added an initial structure logic

---
+++ structure.py
@@ -0,0 +1,130 @@
+######################## TODO: CORPORALIS PERSONA - USER MODULE ########################
+    ### TODO: PERSONAL INFO ###
+        # TODO: ID
+        # TODO: FIRST NAME
+        # TODO: LAST NAME
+        # TODO: DATE OF BIRTH
+        # TODO: COUNTRY
+        # TODO: CITY
+        # TODO: BIOGRAPHY
+        # TODO: CREATION DATE
+        # TODO: IS_VERIFIED
+    ### TODO: RELATIONS ###
+        # TODO: FOLLOWERS
+        # TODO: FOLLOWED
+######################## END OF CORPORALIS PERSONA - USER MODULE ########################
+
+######################## TODO: LEGAL PERSONA - USER MODULE ########################
+    ### TODO: PERSONAL INFO ###
+        # TODO: ID
+        # TODO: NAME
+        # TODO: FOUNDATION DATE
+        # TODO: COUNTRY
+        # TODO: CITY
+        # TODO: BIOGRAPHY
+        # TODO: CREATION DATE
+        # TODO: IS_VERIFIED
+    ### TODO: RELATIONS ###
+        # TODO: FOLLOWERS
+        # TODO: FOLLOWED
+        # TODO: OWNERS
+######################## END OF LEGAL PERSONA - USER MODULE ########################
+
+######################## TODO: IMPERIUM PERSONA - USER MODULE ########################
+    ### TODO: PERSONAL INFO ###
+        # TODO: ID
+        # TODO: NAME
+        # TODO: FOUNDATION DATE
+        # TODO: COUNTRY
+        # TODO: CITY
+        # TODO: BIOGRAPHY
+        # TODO: CREATION DATE
+        # TODO: IS_VERIFIED
+    ### TODO: RELATIONS ###
+        # TODO: FOLLOWERS
+        # TODO: FOLLOWED
+        # TODO: OWNERS
+######################## END OF IMPERIUM PERSONA - USER MODULE ########################
+
+######################## TODO: ADMINISTRADOR PERSONA - USER MODULE ########################
+    ### TODO: INFO ###
+        # TODO: ID
+        # TODO: NAME
+        # TODO: CREATION DATE
+        # TODO: COUNTRY
+        # TODO: CITY
+        # TODO: BIOGRAPHY
+######################## END OF ADMINISTRADOR PERSONA - USER MODULE ########################
+
+######################## TODO: POSTS MODULE ########################
+    # TODO: ID
+    # TODO: POST TITLE
+    # TODO: POST BODY
+    # TODO: RELATIONS
+        # TODO: POST AUTHORS
+        # TODO: POST CREATION DATE
+        # TODO: POST COMMENTS
+        # TODO: IS_POST_SPONSORED
+######################## END OF POSTS MODULE ########################
+
+######################## TODO: COMMENTS MODULE ########################
+    # TODO: ID
+    # TODO: AUTHOR
+    # TODO: SUBCOMMENTS
+    # TODO: DATE
+    # TODO: RELATIONS
+        # TODO: UPVOTES
+        # TODO: DOWNVOTES
+        # TODO: GIFTS
+######################## END OF COMMENTS MODULE ########################
+
+######################## TODO: TABS MODULE ########################
+# TODO: > IF NOT LOGGED IN
+    # TODO: >> LOGIN
+    # TODO: >> REGISTER
+
+# TODO: > IF LOGGED IN
+    # TODO: >> LOG OUT
+    # TODO: >> SEARCH FIELD
+        # TODO: >>> SEARCH ALGORITHM
+            # TODO: >>>> WEBSITE ALGORITHM
+            # TODO: >>>> USER ALGORITHM
+        # TODO: >>> SUBMIT SEARCH
+
+# TODO: > IF ADMIN
+    # TODO: >> ADMIN PANEL
+        # TODO: >>> WEBSITE STATISTICS
+        # TODO: >>> USER CONTROL PANEL
+
+# TODO: > GENERAL TABS
+    # TODO: >> HOME
+    # TODO: >> ABOUT
+
+######################## END OF TABS MODULE ########################
+
+######################## TODO: HOME PAGE ########################
+
+### TODO: FEED ###
+
+# TODO: > SORT FEED BY
+    # TODO: >> LATEST
+        # TODO: >>> VERIFIED USERS
+        # TODO: >>> UNVERIFIED USERS
+
+    # TODO: >> RECOMMENDED (WEBSITE ALGORITHM)
+    # TODO: >> CUSTOM ALGORITHM (USER ALGORITHM)
+
+# TODO: > SHOW POST
+    # TODO: >> IS_VERIFIED?
+    # TODO: >> POST IMAGE
+    # TODO: >> POST VOTES
+    # TODO: >> POST COMMENTS
+    # TODO: >> POST DATE
+
+### END OF FEED ###
+
+######################## END OF HOME PAGE ########################
+
+######################## FOOTER ########################
+
+######################## END OF FOOTER ########################
