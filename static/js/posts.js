3f52001a643eb9c8dc4b60bf59733871b3a8c057
MateusTum <83322634+MateusTum@users.noreply.github.com>
2023-12-28 13:47:15 -0300

    Add Like Animation for Post Interactions
    Implemented an animation to provide visual feedback when users like or unlike a post. This enhancement not only adds a dynamic element to the user interface but also serves to indicate whether a user has already liked a post, improving overall user experience.

--- static/js/posts.js
+++ static/js/posts.js
@@ -22,7 +22,23 @@ $(document).ready(function () {
       url: form.attr("action"),
       data: form.serialize(),
       success: function (response) {
-        likeCountElement.text(response.likes + " Likes");
+        // Create a new SVG element based on the response
+        var newSvgElement = $(
+          '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="' +
+            (response.liked ? "red" : "currentColor") +
+            '" class="bi bi-star-fill likeImage ' +
+            (response.liked ? "liked pop-up-animation-like" : "pop-up-animation-unlike") +
+            '" viewBox="0 0 16 16">' +
+            '<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>' +
+            "</svg>"
+        );
+        form.find(".likeImage").replaceWith(newSvgElement);
+
+        // Update the like count element
+        var newLikeCountElement = $("<span>")
+          .addClass("likeCount")
+          .text(response.likes + " Likes");
+        likeCountElement.replaceWith(newLikeCountElement);
       },
     });
   });
